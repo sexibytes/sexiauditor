ba9775354a08e20839e5328c52a1ade13ba6f381
Frederic MARTIN <frederic@martin.lc>
2016-05-02 17:33:45 +0200

    Update scheduler.pl

--- opt/vcron/scheduler.pl
+++ opt/vcron/scheduler.pl
@@ -253,7 +253,6 @@ if ($purgeThreshold ne 0) {
 VMware::VICredStore::init (filename => $filename) or $logger->logdie ("[ERROR] Unable to initialize Credential Store.");
 @server_list = VMware::VICredStore::get_hosts ();
 foreach $s_item (@server_list) {
-	if (index($s_item, "vctparemea") ne 0){next;}
 	$logger->info("[INFO][VCENTER] Start processing vCenter $s_item");
 	my $normalizedServerName = $s_item;
 	@user_list = VMware::VICredStore::get_usernames (server => $s_item);
