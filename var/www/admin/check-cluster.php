ddbe9dfaff6fcda93e734d7c6d801396f4fb7a41
Frederic MARTIN <frederic@martin.lc>
2016-05-04 17:12:22 +0200

    Update check-cluster.php

--- var/www/admin/check-cluster.php
+++ var/www/admin/check-cluster.php
@@ -29,8 +29,7 @@ try {
 if($check->getModuleSchedule('clusterConfigurationIssues') != 'off' && $check->getModuleSchedule('inventory') != 'off') {
   $check->displayCheck([  'xmlFile' => "clusters-global.xml",
                           'xpathQuery' => "/clusters/cluster[lastconfigissue!='0']",
-                          'title' => 'Cluster with Configuration Issues',
-                          'description' => 'The following clusters have HA configuration issues. This will impact your disaster recovery.',
+                          "id" => "CLUSTERCONFIGURATIONISSUES",
                           'thead' => array('Cluster Name', 'HA Status', 'Last Config Issue', 'Time', 'vCenter'),
                           'tbody' => array('"<td>".$entry->name."</td>"', '"<td>".(($entry->dasenabled == "1") ? "<i class=\"glyphicon glyphicon-remove-sign text-danger\"></i>" : "<i class=\"glyphicon glyphicon-ok-sign text-success\"></i>")."</td>"', '"<td>".$entry->lastconfigissue."</td>"', '"<td>".$entry->lastconfigissuetime."</td>"', '"<td>".$entry->vcenter."</td>"'),
                           'columnDefs' => '{ "orderable": false, className: "dt-body-center", "targets": [ 4 ] }']);
@@ -39,8 +38,7 @@ if($check->getModuleSchedule('clusterConfigurationIssues') != 'off' && $check->g
 if($check->getModuleSchedule('clusterHAStatus') != 'off' && $check->getModuleSchedule('inventory') != 'off') {
   $check->displayCheck([  'xmlFile' => "clusters-global.xml",
                           'xpathQuery' => "/clusters/cluster[dasenabled!='1']",
-                          'title' => 'Cluster Without HA',
-                          'description' => 'The following cluster does not have HA enabled. You should check if that\'s expected as this is a must have feature!',
+                          "id" => "CLUSTERHASTATUS",
                           'thead' => array('Cluster Name', 'HA Status', 'vCenter'),
                           'tbody' => array('"<td>".$entry->name."</td>"', '"<td class=\"text-danger\"><i class=\"glyphicon glyphicon-remove-sign\"></i> no HA</td>"', '"<td>".$entry->vcenter."</td>"')]);
 }
@@ -53,8 +51,7 @@ if($check->getModuleSchedule('clusterHAStatus') != 'off' && $check->getModuleSch
 if($check->getModuleSchedule('clusterMembersVersion') != 'off' && $check->getModuleSchedule('inventory') != 'off') {
   $check->displayCheck([  'xmlFile' => "hosts-global.xml",
                           'xpathQuery' => "/hosts/host",
-                          'title' => 'Hosts Build Number Mismatch',
-                          'description' => 'Display ESX build number by cluster, in order to spot potential intracluster build mismatch.',
+                          "id" => "CLUSTERMEMBERSVERSION",
                           'typeCheck' => 'mismatchPerCluster',
                           'mismatchProperty' => 'esxbuild',
                           'thead' => array('Cluster Name', 'Compliance', 'Build Number', 'vCenter'),
@@ -65,8 +62,7 @@ if($check->getModuleSchedule('clusterMembersVersion') != 'off' && $check->getMod
 if($check->getModuleSchedule('clusterMembersLUNPathCountMismatch') != 'off' && $check->getModuleSchedule('inventory') != 'off') {
   $check->displayCheck([  'xmlFile' => "hosts-global.xml",
                           'xpathQuery' => "/hosts/host",
-                          'title' => 'Cluster With Members LUN Path Count Mismatch',
-                          'description' => 'The following cluster members does not have the same number of LUN, please check for mapping or masking misconfiguration.',
+                          "id" => "CLUSTERMEMBERSLUNPATHCOUNTMISMATCH",
                           'typeCheck' => 'majorityPerCluster',
                           'majorityProperty' => 'lunpathcount',
                           'thead' => array('Cluster Name', 'Majority Path Count', 'Host Name', 'LUN Path Count', 'vCenter'),
@@ -76,8 +72,7 @@ if($check->getModuleSchedule('clusterMembersLUNPathCountMismatch') != 'off' && $
 if($check->getModuleSchedule('clusterCPURatio') != 'off' && $check->getModuleSchedule('inventory') != 'off') {
   $check->displayCheck([  'xmlFile' => "clusters-global.xml",
                           'xpathQuery' => "/clusters/cluster[vp_cpuratio>". $check->getConfig('thresholdCPURatio') ."]",
-                          'title' => 'Ratio Virtual/Physical CPU',
-                          'description' => 'Display ratio of virtual CPU per physical CPU that goes over threshold of ' . $check->getConfig('thresholdCPURatio') . '.',
+                          "id" => "CLUSTERCPURATIO",
                           'thead' => array('Cluster Name', 'pCPU', 'vCPU', 'CPU ratio', 'vCenter'),
                           'tbody' => array('"<td>".$entry->name."</td>"', '"<td>".$entry->pcpu."</td>"', '"<td>".$entry->vcpu."</td>"', '"<td>".$entry->vp_cpuratio."</td>"', '"<td>".$entry->vcenter."</td>"')]);
 }
