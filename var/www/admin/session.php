041e669aa10226e9dba04e4ef52d88a7888b0663
Frederic MARTIN <frederic@martin.lc>
2016-05-04 17:10:17 +0200

    Update session.php

--- var/www/admin/session.php
+++ var/www/admin/session.php
@@ -2,5 +2,6 @@
 # session management file, if not logged, go back to login page
 session_name('SexiAuditor');
 session_start();
+header('Cache-control: private'); // IE 6 FIX
 if (!isset($_SESSION) || !isset($_SESSION['isLogged']) || !$_SESSION['isLogged']) { header('Location: login.php'); }
 ?>
