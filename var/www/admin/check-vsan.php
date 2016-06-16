3292c9c0b3a72c09f8596d0649ad81d7aefd6e96
Frederic MARTIN <frederic@martin.lc>
2016-06-16 14:50:10 +0200

    Create check-vsan.php

---
+++ var/www/admin/check-vsan.php
@@ -0,0 +1,33 @@
+<?php require("session.php"); ?>
+<?php
+$title = "VSAN Checks";
+$additionalStylesheet = array(  'css/jquery.dataTables.min.css',
+                                'css/bootstrap-datetimepicker.css');
+$additionalScript = array(  'js/jquery.dataTables.min.js',
+                            'js/jszip.min.js',
+                            'js/dataTables.autoFill.min.js',
+                            'js/dataTables.bootstrap.min.js',
+                            'js/dataTables.buttons.min.js',
+                            'js/autoFill.bootstrap.min.js',
+                            'js/buttons.bootstrap.min.js',
+                            'js/buttons.colVis.min.js',
+                            'js/buttons.html5.min.js',
+                            'js/file-size.js',
+                            'js/moment.js',
+                            'js/bootstrap-datetimepicker.js');
+require("header.php");
+require("helper.php");
+
+try {
+  # Main class loading
+  $check = new SexiCheck();
+  # Header generation
+  $check->displayHeader($_SERVER['SCRIPT_NAME']);
+} catch (Exception $e) {
+  # Any exception will be ending the script, we want exception-free run
+  exit('  <div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span><span class="sr-only">Error:</span> ' . $e->getMessage() . '</div>');
+}
+
+?>
+	</div>
+<?php require("footer.php"); ?>
