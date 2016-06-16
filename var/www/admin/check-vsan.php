a6dcdb2d366789925c8fc7106c6b2ffa2ae1fa01
Frederic MARTIN <frederic@martin.lc>
2016-06-16 17:24:36 +0200

    Update check-vsan.php

--- var/www/admin/check-vsan.php
+++ var/www/admin/check-vsan.php
@@ -29,5 +29,51 @@ try {
 }
 
 ?>
-	</div>
+    <h1>Hardware compatibility</h1>
+    <h2>Virtual SAN HCL DB up-to-date</h2>
+    <h2>Virtual SAN HCL DB Auto Update</h2>
+    <h2>SCSI Controller on Virtual SAN HCL</h2>
+    <h2>Controller Release Support</h2>
+    <h2>Controller Driver</h2>
+    <h1>Performance service</h1>
+    <h2>Stats DB object</h2>
+    <h1>Network</h1>
+    <h2>Hosts disconnected from VC</h2>
+    <h2>Hosts with connectivity issues</h2>
+    <h2>Virtual SAN cluster partition</h2>
+    <h2>Unexpected Virtual SAN cluster members</h2>
+    <h2>Hosts with Virtual SAN disabled</h2>
+    <h2>All hosts have a Virtual SAN vmknic configured</h2>
+    <h2>All hosts have matching subnets</h2>
+    <h2>All hosts have matching multicast settings</h2>
+    <h2>Basic (unicast) connectivity check (normal ping)</h2>
+    <h2>MTU check (ping with large packet size)</h2>
+    <h2>Multicast assessment based on other checks</h2>
+    <h1>Physical disk</h1>
+    <h2>Overall disks health</h2>
+    <h2>Metadata health</h2>
+    <h2>Disk capacity</h2>
+    <h2>Software state health</h2>
+    <h2>Congestion</h2>
+    <h2>Component limit health</h2>
+    <h2>Component metadata health</h2>
+    <h2>Memory pools (heaps)</h2>
+    <h2>Memory pools (slabs)</h2>
+    <h1>Data</h1>
+    <h2>Virtual SAN object health</h2>
+    <h1>Cluster</h1>
+    <h2>ESX Virtual SAN Health service installation</h2>
+    <h2>Virtual SAN Health Service up-to-date</h2>
+    <h2>Advanced Virtual SAN configuration in sync</h2>
+    <h2>Virtual SAN CLOMD liveness</h2>
+    <h2>Virtual SAN Disk Balance</h2>
+    <h2>Deduplication and compression configuration consistency</h2>
+    <h2>Disk group with incorrect deduplication and compression configuration</h2>
+    <h2>Software version compatibility</h2>
+    <h2>Disk format version</h2>
+    <h1>Limits</h1>
+    <h2>Current cluster situation</h2>
+    <h2>After 1 additional host failure</h2>
+    <h2>Host component limit</h2>
+  </div>
 <?php require("footer.php"); ?>
