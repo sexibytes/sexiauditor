4763c2c67f200c32b3d661dc8a25472e36915b32
Frederic MARTIN <frederic@martin.lc>
2016-08-03 16:19:17 +0200

    Update sexiauditor.sql

--- root/sql/sexiauditor.sql
+++ root/sql/sexiauditor.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u1
+-- version 4.2.12deb2+deb8u2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 01, 2016 at 02:19 PM
--- Server version: 10.0.25-MariaDB-0+deb8u1
--- PHP Version: 5.6.23-0+deb8u1
+-- Generation Time: Aug 03, 2016 at 02:14 PM
+-- Server version: 10.0.26-MariaDB-0+deb8u1
+-- PHP Version: 5.6.24-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -42,6 +42,8 @@ CREATE TABLE IF NOT EXISTS `alarms` (
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `certificates`
 --
@@ -58,6 +60,8 @@ CREATE TABLE IF NOT EXISTS `certificates` (
   `lastseen` datetime NOT NULL,
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `clusters`
@@ -78,6 +82,8 @@ CREATE TABLE IF NOT EXISTS `clusters` (
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `config`
 --
@@ -89,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `config` (
   `type` int(11) NOT NULL,
   `label` varchar(255) CHARACTER SET utf8 NOT NULL,
   `value` varchar(50) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `config`
@@ -104,7 +110,7 @@ INSERT INTO `config` (`id`, `configid`, `type`, `label`, `value`) VALUES
 (6, 'thresholdCPURatio', 4, 'Threshold for vCPU/pCPU ratio check', '2'),
 (7, 'lang', 6, 'Language of appliance', 'en'),
 (8, 'showPlainLicense', 1, 'Display plain license in License Report instead of ''####''', 'disable'),
-(9, 'vcSessionAge', 4, 'Number of days before a session is defined as ''old''', '1'),
+(9, 'vcSessionAge', 4, 'Number of days before a session is defined as ''old''', '7'),
 (10, 'hostSSHPolicy', 5, 'Choose the desired policy for SSH service', 'on'),
 (11, 'hostShellPolicy', 5, 'Choose the desired policy for Shell service', 'on'),
 (12, 'datastoreFreeSpaceThreshold', 4, 'Datastore free space threshold %', '30'),
@@ -112,9 +118,9 @@ INSERT INTO `config` (`id`, `configid`, `type`, `label`, `value`) VALUES
 (14, 'networkDVSVSSportsfree', 4, 'Threshold of free port for DVS needed', '123'),
 (15, 'vmSnapshotAge', 4, 'Number of days before a snapshot is defined as ''old''', '98'),
 (16, 'timeToBuildCount', 4, 'Number of entries ''Time To Build'' page will display (0 for all)', '100'),
-(17, 'smtpAddress', 2, 'SMTP server address (IP, or FQDN) to forward email to', 'smtp'),
-(18, 'senderMail', 3, 'Sender email to be used for report export feature', 'user@domain.tld'),
-(19, 'recipientMail', 3, 'Recipient email to be used for report export feature', 'user@domain.tld'),
+(17, 'smtpAddress', 2, 'SMTP server address (IP, or FQDN) to forward email to', 'smtp-goss.fr.world.socgen'),
+(18, 'senderMail', 3, 'Sender email to be used for report export feature', 'BCO_VCenter-2@socgen.com'),
+(19, 'recipientMail', 3, 'Recipient email to be used for report export feature', 'frederic.c.martin-ext@socgen.com'),
 (20, 'pdfAuthor', 2, 'Username that will be used as the ''Author'' of generated PDFs', 'Gordon Freeman'),
 (21, 'showEmpty', 1, 'Show checks that return empty values (ie when there is nothing to report)', 'enable'),
 (22, 'showAuthors', 1, 'Show authors page of generated PDF''s, be aware that disabling it may kill some kitten...', 'enable');
@@ -129,7 +135,7 @@ DROP TABLE IF EXISTS `configtype`;
 CREATE TABLE IF NOT EXISTS `configtype` (
 `id` int(11) NOT NULL,
   `type` varchar(50) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `configtype`
@@ -163,6 +169,8 @@ CREATE TABLE IF NOT EXISTS `configurationissues` (
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `datastores`
 --
@@ -186,6 +194,8 @@ CREATE TABLE IF NOT EXISTS `datastores` (
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `distributedvirtualportgroups`
 --
@@ -204,6 +214,8 @@ CREATE TABLE IF NOT EXISTS `distributedvirtualportgroups` (
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `executiontime`
 --
@@ -214,6 +226,8 @@ CREATE TABLE IF NOT EXISTS `executiontime` (
   `date` datetime NOT NULL,
   `seconds` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `hardwarestatus`
@@ -230,6 +244,8 @@ CREATE TABLE IF NOT EXISTS `hardwarestatus` (
   `lastseen` datetime NOT NULL,
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `hosts`
@@ -267,6 +283,8 @@ CREATE TABLE IF NOT EXISTS `hosts` (
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `licenses`
 --
@@ -285,6 +303,8 @@ CREATE TABLE IF NOT EXISTS `licenses` (
   `lastseen` datetime NOT NULL,
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `moduleCategory`
@@ -335,24 +355,24 @@ INSERT INTO `modules` (`id`, `module_id`, `type`, `displayName`, `version`, `des
 (1, 'VSANHealthCheck', 'report', 'VSAN Health Check', '0', 'Display VSAN information about Health Check.', 1),
 (2, 'vcSessionAge', 'report', 'Session Age', '1', 'Display vCenter session that are older than (x)days.', 2),
 (3, 'vcLicenceReport', 'report', 'License Report', '1', 'Display licence consumption based on vCenter licence defined.', 2),
-(4, 'vcPermissionReport', 'report', 'Permission report', '0', 'Display permission listing with user/role combination.', 2),
-(5, 'vcTerminateSession', 'action', 'Terminate session', '0', '[Action] Kill vCenter session older than (x)days.', 2),
-(6, 'vcCertificatesReport', 'report', 'Certificate Report', '0', 'Display soon-to-be-expired certificates for all vCenter components (SSO, WebClient, ...).', 2),
+(4, 'vcPermissionReport', 'report', 'Permission report', '1', 'Display permission listing with user/role combination.', 2),
+(5, 'vcTerminateSession', 'action', 'Terminate session', '1', '[Action] Kill vCenter session older than (x)days.', 2),
+(6, 'vcCertificatesReport', 'report', 'Certificate Report', '1', 'Display soon-to-be-expired certificates for all vCenter components (SSO, WebClient, ...).', 2),
 (7, 'clusterConfigurationIssues', 'report', 'Configuration Issues', '1', 'Configuration Issues report.', 3),
 (8, 'clusterAdmissionControl', 'report', 'Admission Control', '0', 'Admission Control report.', 3),
 (9, 'clusterHAStatus', 'report', 'Cluster Without HA', '1', 'Display cluster without HA.', 3),
 (10, 'clusterDatastoreConsistency', 'report', 'Datastore Consistency', '0', 'Datastore Consistency report.', 3),
 (11, 'clusterMembersVersion', 'report', 'Members Version', '1', 'Members Version Report.', 3),
 (12, 'clusterMembersOvercommit', 'report', 'Members Overcommit', '0', 'Members Overcommit Report.', 3),
-(13, 'clusterMembersLUNPathCountMismatch', 'report', 'Members LUN Path count mismatch', '0', 'Members LUN Path count mismatch Report.', 3),
+(13, 'clusterMembersLUNPathCountMismatch', 'report', 'Members LUN Path count mismatch', '1', 'Members LUN Path count mismatch Report.', 3),
 (14, 'clusterCPURatio', 'report', 'vCPU pCPU table', '1', 'vCPU pCPU table Report.', 3),
 (15, 'clusterTPSSavings', 'report', 'TPS savings', '0', 'TPS savings Report.', 3),
 (16, 'clusterAutoSlotSize', 'report', 'AutoSlotSize', '0', 'AutoSlotSize Report.', 3),
 (17, 'clusterProfile', 'report', 'Profile', '0', 'Profile Report.', 3),
-(18, 'hostLUNPathDead', 'report', 'LUN Path Dead', '0', 'LUN Path Dead report.', 4),
+(18, 'hostLUNPathDead', 'report', 'LUN Path Dead', '1', 'LUN Path Dead report.', 4),
 (19, 'hostProfileCompliance', 'report', 'Profile Compliance', '0', 'Profile Compliance report.', 4),
 (20, 'hostLocalSwapDatastoreCompliance', 'report', 'LocalSwapDatastore Compliance', '0', 'LocalSwapDatastore Compliance report.', 4),
-(21, 'hostSshShell', 'report', 'SSH/shell check', '0', 'SSH/shell check report.', 4),
+(21, 'hostSshShell', 'report', 'SSH/shell check', '1', 'SSH/shell check report.', 4),
 (22, 'hostNTPCheck', 'report', 'NTP Check', '1', 'NTP Check report.', 4),
 (23, 'hostDNSCheck', 'report', 'DNS Check', '1', 'DNS Check report.', 4),
 (24, 'hostSyslogCheck', 'report', 'Syslog Check', '1', 'Syslog Check report.', 4),
@@ -362,7 +382,7 @@ INSERT INTO `modules` (`id`, `module_id`, `type`, `displayName`, `version`, `des
 (28, 'hostFQDNHostnameMismatch', 'report', 'FQDN/hostname mismatch', '1', 'FQDN/hostname mismatch report.', 4),
 (29, 'hostMaintenanceMode', 'report', 'Maintenance Mode', '1', 'maintenance mode report.', 4),
 (30, 'hostballooningzipswap', 'report', 'ballooning/zip/swap', '0', 'ballooning/zip/swap report.', 4),
-(31, 'hostPowerManagementPolicy', 'report', 'PowerManagement Policy', '0', 'PowerManagement Policy report.', 4),
+(31, 'hostPowerManagementPolicy', 'report', 'PowerManagement Policy', '1', 'PowerManagement Policy report.', 4),
 (32, 'hostBundlebackup', 'action', 'Bundle backup', '0', 'Bundle backup report.', 4),
 (33, 'datastoreSpacereport', 'report', 'Space report', '1', 'Space report.', 5),
 (34, 'datastoreOrphanedVMFilesreport', 'report', 'Orphaned VM Files report', '0', 'Orphaned VM Files report report.', 5),
@@ -412,68 +432,87 @@ CREATE TABLE IF NOT EXISTS `moduleSchedule` (
 --
 
 INSERT INTO `moduleSchedule` (`id`, `schedule`) VALUES
-('alarms', 'off'),
+('alarms', 'daily'),
+('clusterConfigurationIssues', 'daily'),
+('clusterCPURatio', 'daily'),
+('clusterHAStatus', 'daily'),
+('clusterMembersLUNPathCountMismatch', 'daily'),
+('clusterMembersVersion', 'daily'),
+('datastoreAccessible', 'daily'),
+('datastoremaintenancemode', 'daily'),
+('datastoreOverallocation', 'daily'),
+('datastoreSIOCdisabled', 'daily'),
+('datastoreSpacereport', 'daily'),
+('hostConfigurationIssues', 'daily'),
+('hostDNSCheck', 'daily'),
+('hostFQDNHostnameMismatch', 'daily'),
+('hostHardwareStatus', 'daily'),
+('hostLUNPathDead', 'daily'),
+('hostMaintenanceMode', 'daily'),
+('hostNTPCheck', 'daily'),
+('hostPowerManagementPolicy', 'daily'),
+('hostRebootrequired', 'daily'),
+('hostSshShell', 'daily'),
+('hostSyslogCheck', 'daily'),
+('networkDVSportsfree', 'daily'),
+('vcCertificatesReport', 'daily'),
+('vcLicenceReport', 'daily'),
+('vcPermissionReport', 'daily'),
+('vcSessionAge', 'daily'),
+('vcTerminateSession', 'daily'),
+('vmballoonzipswap', 'daily'),
+('vmconsolidationneeded', 'daily'),
+('vmcpuramhddlimits', 'daily'),
+('vmcpuramhddreservation', 'daily'),
+('vmcpuramhotadd', 'daily'),
+('vmGuestIdMismatch', 'daily'),
+('vmGuestPivot', 'daily'),
+('vmInconsistent', 'daily'),
+('vmInvalidOrInaccessible', 'daily'),
+('vmMisnamed', 'daily'),
+('vmmultiwritermode', 'daily'),
+('vmNonpersistentmode', 'daily'),
+('vmphantomsnapshot', 'daily'),
+('vmPoweredOff', 'daily'),
+('vmRemovableConnected', 'daily'),
+('vmscsibussharing', 'daily'),
+('vmSnapshotsage', 'daily'),
+('vmToolsPivot', 'daily'),
+('vmvHardwarePivot', 'daily'),
+('inventory', 'hourly'),
 ('clusterAdmissionControl', 'off'),
 ('clusterAutoSlotSize', 'off'),
-('clusterConfigurationIssues', 'off'),
-('clusterCPURatio', 'off'),
 ('clusterDatastoreConsistency', 'off'),
-('clusterHAStatus', 'off'),
-('clusterMembersLUNPathCountMismatch', 'off'),
 ('clusterMembersOvercommit', 'off'),
-('clusterMembersVersion', 'off'),
 ('clusterProfile', 'off'),
 ('clusterTPSSavings', 'off'),
-('datastoreAccessible', 'off'),
-('datastoremaintenancemode', 'off'),
 ('datastoreOrphanedVMFilesreport', 'off'),
-('datastoreOverallocation', 'off'),
-('datastoreSIOCdisabled', 'off'),
-('datastoreSpacereport', 'off'),
 ('hostballooningzipswap', 'off'),
-('hostConfigurationIssues', 'off'),
-('hostDNSCheck', 'off'),
-('hostFQDNHostnameMismatch', 'off'),
-('hostHardwareStatus', 'off'),
-('hostLocalSwapDatastoreCompliance', 'off'),
-('hostLUNPathDead', 'off'),
-('hostMaintenanceMode', 'off'),
-('hostNTPCheck', 'off'),
-('hostPowerManagementPolicy', 'off'),
-('hostProfileCompliance', 'off'),
-('hostRebootrequired', 'off'),
-('hostSshShell', 'off'),
-('hostSyslogCheck', 'off'),
-('networkDVSportsfree', 'off'),
-('networkDVSprofile', 'off'),
-('vcCertificatesReport', 'off'),
-('vcLicenceReport', 'off'),
-('vcSessionAge', 'off'),
-('vmballoonzipswap', 'off'),
-('vmconsolidationneeded', 'off'),
-('vmcpuramhddlimits', 'off'),
-('vmcpuramhddreservation', 'off'),
-('vmcpuramhotadd', 'off'),
-('vmGuestIdMismatch', 'off'),
-('vmGuestPivot', 'off'),
-('vmInconsistent', 'off'),
-('vmInvalidOrInaccessible', 'off'),
-('vmMisnamed', 'off'),
-('vmmultiwritermode', 'off'),
-('vmNonpersistentmode', 'off'),
-('vmphantomsnapshot', 'off'),
-('vmPoweredOff', 'off'),
-('vmRemovableConnected', 'off'),
-('vmscsibussharing', 'off'),
-('vmSnapshotsage', 'off'),
-('vmToolsPivot', 'off'),
-('vmvHardwarePivot', 'off'),
-('VSANHealthCheck', 'off'),
-('inventory', 'hourly'),
 ('hostBundlebackup', 'off'),
+('hostLocalSwapDatastoreCompliance', 'off'),
+('hostProfileCompliance', 'off'),
 ('networkDVPGAutoExpand', 'off'),
-('vcPermissionReport', 'off'),
-('vcTerminateSession', 'off');
+('networkDVSprofile', 'off'),
+('VSANHealthCheck', 'off');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+CREATE TABLE IF NOT EXISTS `permissions` (
+`id` int(11) NOT NULL,
+  `vcenter` int(11) NOT NULL,
+  `principal` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `role_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `isGroup` tinyint(1) NOT NULL,
+  `inventory_path` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `firstseen` datetime NOT NULL,
+  `lastseen` datetime NOT NULL,
+  `active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -516,6 +555,8 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `snapshots`
 --
@@ -535,6 +576,8 @@ CREATE TABLE IF NOT EXISTS `snapshots` (
   `lastseen` datetime NOT NULL,
   `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
@@ -572,6 +615,8 @@ CREATE TABLE IF NOT EXISTS `vcenters` (
   `lastseen` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `vms`
 --
@@ -579,6 +624,7 @@ CREATE TABLE IF NOT EXISTS `vcenters` (
 DROP TABLE IF EXISTS `vms`;
 CREATE TABLE IF NOT EXISTS `vms` (
 `id` int(11) NOT NULL,
+  `vcenter` int(11) NOT NULL,
   `host` int(11) NOT NULL,
   `memReservation` int(11) NOT NULL,
   `guestFamily` varchar(255) CHARACTER SET utf8 NOT NULL,
@@ -707,13 +753,19 @@ ALTER TABLE `moduleCategory`
 -- Indexes for table `modules`
 --
 ALTER TABLE `modules`
- ADD PRIMARY KEY (`id`), ADD KEY `module_id` (`module_id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `module﻿_id` (`module_id`);
 
 --
 -- Indexes for table `moduleSchedule`
 --
 ALTER TABLE `moduleSchedule`
  ADD PRIMARY KEY (`id`), ADD KEY `schedule` (`schedule`);
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+ ADD PRIMARY KEY (`id`), ADD KEY `vcenter` (`vcenter`);
 
 --
 -- Indexes for table `roles`
@@ -749,7 +801,7 @@ ALTER TABLE `vcenters`
 -- Indexes for table `vms`
 --
 ALTER TABLE `vms`
- ADD PRIMARY KEY (`id`), ADD KEY `host` (`host`);
+ ADD PRIMARY KEY (`id`), ADD KEY `host` (`host`), ADD KEY `vcenter` (`vcenter`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -774,7 +826,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `config`
 --
 ALTER TABLE `config`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT for table `configtype`
 --
@@ -826,6 +878,11 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 ALTER TABLE `modules`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=63;
 --
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
@@ -860,52 +917,10 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 
 --
--- Constraints for table `alarms`
---
-ALTER TABLE `alarms`
-ADD CONSTRAINT `alarms_ibfk_1` FOREIGN KEY (`vcenter`) REFERENCES `vcenters` (`id`);
-
---
--- Constraints for table `certificates`
---
-ALTER TABLE `certificates`
-ADD CONSTRAINT `certificates_ibfk_1` FOREIGN KEY (`vcenter`) REFERENCES `vcenters` (`id`);
-
---
--- Constraints for table `clusters`
---
-ALTER TABLE `clusters`
-ADD CONSTRAINT `clusters_ibfk_1` FOREIGN KEY (`vcenter`) REFERENCES `vcenters` (`id`);
-
---
 -- Constraints for table `config`
 --
 ALTER TABLE `config`
 ADD CONSTRAINT `config_ibfk_1` FOREIGN KEY (`type`) REFERENCES `configtype` (`id`);
-
---
--- Constraints for table `datastores`
---
-ALTER TABLE `datastores`
-ADD CONSTRAINT `datastores_ibfk_1` FOREIGN KEY (`vcenter`) REFERENCES `vcenters` (`id`);
-
---
--- Constraints for table `hosts`
---
-ALTER TABLE `hosts`
-ADD CONSTRAINT `hosts_ibfk_1` FOREIGN KEY (`vcenter`) REFERENCES `vcenters` (`id`);
-
---
--- Constraints for table `licenses`
---
-ALTER TABLE `licenses`
-ADD CONSTRAINT `licenses_ibfk_1` FOREIGN KEY (`vcenter`) REFERENCES `vcenters` (`id`);
-
---
--- Constraints for table `sessions`
---
-ALTER TABLE `sessions`
-ADD CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`vcenter`) REFERENCES `vcenters` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
