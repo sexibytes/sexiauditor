5da6020e5285e6dc5e1cad05112fe270aae93e10
Frederic MARTIN <frederic@martin.lc>
2016-08-19 17:25:55 +0200

    Update capacityplanning.php

--- var/www/admin/capacityplanning.php
+++ var/www/admin/capacityplanning.php
@@ -4,7 +4,8 @@ $title = "Capacity Planning";
 $additionalStylesheet = array(  'css/jquery.dataTables.min.css',
                                 'css/whhg.css',
                                 'css/bootstrap-datetimepicker.css');
-$additionalScript = array(  'js/jquery.dataTables.min.js',
+$additionalScript = array(  'js/highcharts.js',
+                            'js/jquery.dataTables.min.js',
                             'js/jszip.min.js',
                             'js/dataTables.autoFill.min.js',
                             'js/dataTables.bootstrap.min.js',
@@ -29,7 +30,17 @@ try {
   exit('  <div class="alert alert-danger" role="alert"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span><span class="sr-only">Error:</span> ' . $e->getMessage() . '</div>');
 }
 ?>
-    <table id="capacityplanning" class="display table" cellspacing="0" width="100%">
+    <style>
+    .highcharts-tooltip>span {
+      background: white;
+      border: 1px solid silver;
+      border-radius: 3px;
+      box-shadow: 1px 1px 2px #888;
+      padding: 8px;
+    }
+    </style>
+
+    <table id="table-sparkline" class="display table" cellspacing="0" width="100%">
       <thead>
         <th>Cluster</th>
         <th>vCenter</th>
@@ -37,12 +48,13 @@ try {
         <th>Days Left</th>
         <th>Trend</th>
       </thead>
-      <tbody>
+      <tbody id="tbody-sparkline">
 <?php
 $sexigrafNode = $check->getConfig('sexigrafNode');
 $capacityPlanningDays = (int)$check->getConfig('capacityPlanningDays');
 $showInfinite = $check->getConfig('showInfinite');
 $jsonVC = json_decode(file_get_contents("http://$sexigrafNode:8080/metrics/find?query=vmw.*"), TRUE);
+$javascriptCode = "";
 foreach ($jsonVC as $entryVC) {
   $vcenter = $entryVC['text'];
   $vcenterID = $entryVC['id'];
@@ -57,12 +69,28 @@ foreach ($jsonVC as $entryVC) {
       $urlCapaPlan = "http://$sexigrafNode:8080/render?target=scale(diffSeries(divideSeries(scale(sumSeries($clusterID.runtime.vm.on),100),maxSeries(asPercent(sumSeries($clusterID.quickstats.cpu.usage),sumSeries($clusterID.quickstats.cpu.effective)),asPercent(sumSeries($clusterID.quickstats.mem.usage),sumSeries($clusterID.quickstats.mem.effective)))),sumSeries($clusterID.runtime.vm.on)),1)";
       $optionsJson = "&from=-".$capacityPlanningDays."days&format=json";
       $dataCapaPlan = json_decode(file_get_contents($urlCapaPlan.$optionsJson), TRUE)[0]["datapoints"];
-      $vmLeftT0 = round($dataCapaPlan[0][0],0);
-      $vmLeftT1 = round(array_slice($dataCapaPlan,-2)[0][0],0);
+      $dataCapaPlanInversed = array();
+      foreach ($dataCapaPlan as $tmpDataCapaPlan) {
+        $dataCapaPlanInversed[] = (int)round($tmpDataCapaPlan[0]);
+      }
+      $maxDataPoint = 50;
+      $newDataCapaPlanInversed = array();
+      $stepDataPoint = 1;
+      if (count($dataCapaPlanInversed) > $maxDataPoint) {
+        $stepDataPoint = floor(count($dataCapaPlanInversed)/$maxDataPoint);
+      } else {
+        $maxDataPoint = count($dataCapaPlanInversed);
+      }
+      for ($i = 1; $i < $maxDataPoint; $i++) {
+        $newDataCapaPlanInversed[] = $dataCapaPlanInversed[$stepDataPoint * $i];
+      }
+      $newDataCapaPlanInversed[] = $dataCapaPlanInversed[count($dataCapaPlanInversed)-2];
+      $vmLeftT0 = $dataCapaPlanInversed[0];
+      $vmLeftT1 = $dataCapaPlanInversed[count($dataCapaPlanInversed)-2];
       $coefficientCapaPlan = ($vmLeftT1-$vmLeftT0)/$capacityPlanningDays;
       if ($coefficientCapaPlan < 0) {
         $daysLeft = round(abs($vmLeftT1/$coefficientCapaPlan),0);
-        $colorLine = "red";
+        $colorLine = "#910000";
       } else {
         $daysLeft = "<i class=\"icon-infinityalt\"></i>";
         $colorLine = "green";
@@ -74,14 +102,13 @@ foreach ($jsonVC as $entryVC) {
       } else {
         $optionsTime = "&from=-".$capacityPlanningDays."days";
       }
-      $optionsImage = $optionsTime."&hideLegend=true&width=500&height=50&hideAxes=true&bgcolor=FFFFFF00&colorList=$colorLine";
       if ($coefficientCapaPlan < 0 || $showInfinite == 'enable') {
         echo "      <tr>\n";
         echo "        <th>$cluster</th>\n";
         echo "        <td>$vcenter</td>\n";
-        echo "        <td>$vmLeftT0</td>\n";
+        echo "        <td>$vmLeftT1</td>\n";
         echo "        <td>$daysLeft</td>\n";
-        echo '        <td><img src="data:image/png;base64,' . base64_encode(file_get_contents($urlCapaPlan.$optionsImage)) . '"></td>'."\n";
+        echo '        <td data-sparkline="' . implode(", ", $newDataCapaPlanInversed) . '" data-sparkline-color="'. $colorLine . '"/>'."\n";
         echo "      </tr>\n";
       }
     }
@@ -90,10 +117,128 @@ foreach ($jsonVC as $entryVC) {
 ?>
       <tbody>
     </table>
-
+    
     <script type="text/javascript">
+      $(function () {
+        /**
+         * Create a constructor for sparklines that takes some sensible defaults and merges in the individual
+         * chart options. This function is also available from the jQuery plugin as $(element).highcharts('SparkLine').
+         */
+        Highcharts.SparkLine = function (a, b, c) {
+          var hasRenderToArg = typeof a === 'string' || a.nodeName,
+          options = arguments[hasRenderToArg ? 1 : 0],
+          defaultOptions = {
+            chart: {
+              renderTo: (options.chart && options.chart.renderTo) || this,
+              backgroundColor: null,
+              borderWidth: 0,
+              type: 'area',
+              margin: [2, 0, 2, 0],
+              width: 500,
+              height: 50,
+              style: { overflow: 'visible' },
+              skipClone: true
+            },
+            title: { text: '' },
+            credits: { enabled: false },
+            xAxis: {
+              labels: { enabled: false },
+              title: { text: null },
+              startOnTick: false,
+              endOnTick: false,
+              tickPositions: []
+            },
+            yAxis: {
+              endOnTick: false,
+              startOnTick: false,
+              labels: { enabled: false },
+              title: { text: null },
+              tickPositions: [0]
+            },
+            legend: { enabled: false },
+            tooltip: {
+              backgroundColor: null,
+              borderWidth: 0,
+              shadow: false,
+              useHTML: true,
+              hideDelay: 0,
+              shared: true,
+              padding: 0,
+              positioner: function (w, h, point) { return { x: point.plotX - w / 2, y: point.plotY - h }; }
+            },
+            plotOptions: {
+              series: {
+                animation: false,
+                lineWidth: 1,
+                shadow: false,
+                states: { hover: { lineWidth: 1 } },
+                marker: {
+                  radius: 1,
+                  states: { hover: { radius: 2 } }
+                },
+                fillOpacity: 0.25
+              }
+            }
+          };
+          options = Highcharts.merge(defaultOptions, options);
+          return hasRenderToArg ? new Highcharts.Chart(a, options, c) : new Highcharts.Chart(options, b);
+        };
+
+        var start = +new Date(),
+        $tds = $('td[data-sparkline]'),
+        fullLen = $tds.length,
+        n = 0;
+
+        // Creating 153 sparkline charts is quite fast in modern browsers, but IE8 and mobile
+        // can take some seconds, so we split the input into chunks and apply them in timeouts
+        // in order avoid locking up the browser process and allow interaction.
+        function doChunk() {
+          var time = +new Date(),
+          i,
+          len = $tds.length,
+          $td,
+          stringdata,
+          arr,
+          data,
+          chart;
+
+          for (i = 0; i < len; i += 1) {
+            $td = $($tds[i]);
+            stringdata = $td.data('sparkline');
+            arr = stringdata.split('; ');
+            data = $.map(arr[0].split(', '), parseFloat);
+            chart = {};
+            if (arr[1]) { chart.type = arr[1]; }
+            $td.highcharts('SparkLine', {
+              series: [{
+                data: data,
+                pointStart: 1
+              }],
+              plotOptions: {
+                series: {
+                  color: $td.data('sparkline-color')
+                }
+              },
+              tooltip: {
+                headerFormat: '<span>' + $td.parent().find('th').html() + '</span><br/>',
+                pointFormat: '<b>{point.y}</b> VM left'
+              },
+              chart: chart
+            });
+            n += 1;
+            // If the process takes too much time, run a timeout to allow interaction with the browser
+            if (new Date() - time > 500) {
+              $tds.splice(0, i + 1);
+              setTimeout(doChunk, 0);
+              break;
+            }
+          }
+        }
+        doChunk();
+      });
+      
       $(document).ready( function () {
-        var table = $('#capacityplanning').DataTable( {
+        var table = $('#table-sparkline').DataTable( {
           "language": { "infoFiltered": "" },
           "search": {
             "smart": false,
