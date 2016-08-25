8a5c842f58859068892d4c466a8eeee5cdf1e18a
Frederic MARTIN <frederic@martin.lc>
2016-08-25 12:29:47 +0200

    Update dbconnection.php

--- var/www/admin/dbconnection.php
+++ var/www/admin/dbconnection.php
@@ -8,7 +8,10 @@ $db = new MysqliDb ($dbServer, $dbUername, $dbPassword, $dbDabatase);
 try {
   $db->connect();
 } catch (Exception $e) {
-  header("Location: exception.php?e=dbcon");
+  # Any exception will be ending the script, we want exception-free run
+  # CSS hack for navbar margin removal
+  echo '  <style>#wrapper { margin-bottom: 0px !important; }</style>'."\n";
+  require("exception.php");
   exit;
 }
 ?>
