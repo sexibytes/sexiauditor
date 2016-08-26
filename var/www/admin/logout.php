34f2bae9070905f6afe18329a5655cac368d61a1
Frederic MARTIN <frederic@martin.lc>
2016-08-26 14:50:41 +0200

    Update logout.php

--- var/www/admin/logout.php
+++ var/www/admin/logout.php
@@ -1,7 +1,9 @@
-<?php require("session.php"); ?>
 <?php
-	session_unset();
-	session_destroy ();
-	session_write_close();
-	header('Location: login.php');
+# We destroy all session object/files
+require("session.php");
+session_unset();
+session_destroy ();
+session_write_close();
+header('Location: login.php');
+exit;
 ?>
