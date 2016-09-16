9f2b4d2843c44511998a349915d59b66429f7f35
Frederic MARTIN <frederic@martin.lc>
2016-09-16 10:27:58 +0200

    Avoid duplicates

--- var/www/admin/search.php
+++ var/www/admin/search.php
@@ -1,8 +1,12 @@
 <?php
 require_once ('dbconnection.php');
-$cols = Array ("id", "'ESX' AS type", "CONCAT('showhost.php?hostid=', id) as urlid", "host_name as name");
-$db->where('host_name', '%' . $_GET['query'] . '%', 'LIKE');
-$esxhosts = $db->get("hosts", 5, $cols);
+
+$esxQ = $db->subQuery("hid");
+$esxQ->where('host_name', '%' . $_GET['query'] . '%', 'LIKE');
+$esxQ->groupBy("moref, vcenter");
+$esxQ->get("hosts", null, "MAX(id) as hostid");
+$db->join($esxQ, "hid.hostid = hosts.id", "INNER");
+$esxhosts = $db->get("hosts", 5, array("id", "'ESX' AS type", "CONCAT('showhost.php?hostid=', id) as urlid", "host_name as name"));
 
 // populate ESX results
 $results = array();
@@ -10,21 +14,27 @@ foreach ($esxhosts as $row) {
     $results[] = $row;
 }
 
-$cols = Array ("id", "'VM' AS type", "CONCAT('showvm.php?vmid=', id) as urlid", "name");
-$db->where('name', '%' . $_GET['query'] . '%', 'LIKE');
-$vms = $db->get("vms", 5, $cols);
+$vmQ = $db->subQuery("vid");
+$vmQ->where('name', '%' . $_GET['query'] . '%', 'LIKE');
+$vmQ->groupBy("moref, vcenter");
+$vmQ->get("vms", null, "MAX(id) as vmid");
+$db->join($vmQ, "vid.vmid = vms.id", "INNER");
+$vms = $db->get("vms", 5, array("id", "'VM' AS type", "CONCAT('showvm.php?vmid=', id) as urlid", "name"));
 
 // populate VM results
 foreach ($vms as $row) {
     $results[] = $row;
 }
 
-$cols = Array ("id", "'DS' AS type", "CONCAT('showdatastore.php?dsid=', id) as urlid", "datastore_name as name");
-$db->where('datastore_name', '%' . $_GET['query'] . '%', 'LIKE');
-$vms = $db->get("datastores", 5, $cols);
+$dsQ = $db->subQuery("did");
+$dsQ->where('datastore_name', '%' . $_GET['query'] . '%', 'LIKE');
+$dsQ->groupBy("moref, vcenter");
+$dsQ->get("datastores", null, "MAX(id) as dsid");
+$db->join($dsQ, "did.dsid = datastores.id", "INNER");
+$ds = $db->get("datastores", 5, array("id", "'DS' AS type", "CONCAT('showdatastore.php?dsid=', id) as urlid", "datastore_name as name"));
 
 // populate datastore results
-foreach ($vms as $row) {
+foreach ($ds as $row) {
     $results[] = $row;
 }
 
