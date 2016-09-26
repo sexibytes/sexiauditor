336a61f721cbc1c601d7359f2a59e8d13e63e7d7
Frederic MARTIN <frederic@martin.lc>
2016-09-26 15:14:43 +0200

    Add files via upload

---
+++ var/www/admin/about.php
@@ -0,0 +1,15 @@
+<?php
+require("session.php");
+$title = "About SexiAuditor";
+require("header.php");
+?>
+  <div class="container">
+    <ul>
+      <li>what is it</li>
+      <li>history</li>
+      <li>authors</li>
+      <li>library used</li>
+      <li>contact</li>
+    </ul>
+  </div>
+<?php require("footer.php"); ?>
--- var/www/admin/pdfreports.php
+++ var/www/admin/pdfreports.php
@@ -1,25 +1,55 @@
-<?php require("session.php"); ?>
 <?php
+require("session.php");
 $title = "PDF Reports";
-$additionalStylesheet = array(  'css/jquery.dataTables.min.css');
-$additionalScript = array(  'js/jquery.dataTables.min.js',
-                            'js/jszip.min.js',
-                            'js/dataTables.autoFill.min.js',
-                            'js/dataTables.bootstrap.min.js',
-                            'js/dataTables.buttons.min.js',
-                            'js/autoFill.bootstrap.min.js',
-                            'js/buttons.bootstrap.min.js',
-                            'js/buttons.colVis.min.js',
-                            'js/buttons.html5.min.js',
-                            'js/moment.js',
-                            'js/datetime-moment.js',
-                            'js/file-size.js');
+$additionalStylesheet = array('css/pong.css');
+// $additionalScript = array(  'js/jquery.dataTables.min.js',
+//                             'js/jszip.min.js',
+//                             'js/dataTables.autoFill.min.js',
+//                             'js/dataTables.bootstrap.min.js',
+//                             'js/dataTables.buttons.min.js',
+//                             'js/autoFill.bootstrap.min.js',
+//                             'js/buttons.bootstrap.min.js',
+//                             'js/buttons.colVis.min.js',
+//                             'js/buttons.html5.min.js',
+//                             'js/moment.js',
+//                             'js/datetime-moment.js',
+//                             'js/file-size.js');
 require("header.php");
 require("helper.php");
 ?>
   <div class="container">
     <h1><i class="glyphicon glyphicon-print"></i> <?php echo $title; ?></h1>
-    <h2>One Time Generation</h2>
+    blabla coming soon
+    in the mea time > pong html5
+    
+  </div>
+    <div id="gamediv">
+	<div id="titleScreen">
+		<h1>Pong!</h1>
+		<p>This game is based on the HTML5 elements <b>canvas</b> and <b>audio</b>. With some simple javascript to make it work.</p>
+		<p>To play this game you need a modern web browser with support for HTML5. Audio only works with Firefox 3.5+ at the moment.</p>
+		<p class="vcard">Made by <a href="http://daverix.net/" class="url fn" target="_top" rel="me">David Laurell</a></p>
+		<button id="playButton">Play!</button>
+	</div>
+	<div id="playScreen">
+		<canvas width="640" height="360" id="gameCanvas">
+			<p>Your browser <b>does not</b> support HTML5!</p>
+			<p>Download <a href="http://firefox.com">Firefox3.6</a> for the full experience or another with good HTML5 support. The game is tested in Firefox 3.0+, Chromium 4+, Chrome 4 beta, Opera and Internet Explorer 8. To get the audio to work you are required to use Firefox for the moment.</p>
+			<p>Visit the <a href="http://daverix.net/projects/pong/">project page</a> for more info.</p>
+		</canvas>
+		<div id="computerScore">0</div>
+		<div id="playerScore">0</div>
+		<div class="ingamebuttons">
+			<button id="pauseButton">Pause</button>
+			<!-- <button id="soundButton">Turn off sound</button> -->
+		</div>
+		<div id="pauseText">Paused</div>
+	</div>
+</div>
+
+    
+    
+    <!-- <h2>One Time Generation</h2>
     <div class="col-lg-12">
       To generate a new global report and display it, please push <del>gently</del> right here:
       <a href="pdfgenerate.php" class="btn btn-primary" style="margin:10px;">Generate Report</a>
@@ -32,47 +62,47 @@ require("helper.php");
         <th class="col-sm-2 text-right">Size</th>
         <th class="col-sm-2 text-right">Modified</th>
       </thead>
-      <tbody>
+      <tbody> -->
 <?php
-  $dir = "/var/www/admin/reports/";
-  if ($handle = opendir($dir)) {
-    $numReport = 1;
-    while (false !== ($file = readdir($handle))) {
-      if ($file != "." && $file != ".." && $file != ".gitignore") {
-        $lastModified = date('F d Y, H:i:s',filemtime($dir.$file));
-        echo '            <tr><td class="text-left"><i class="glyphicon glyphicon-file"></i> <a href="#report'.$numReport.'" data-toggle="modal" rel="external">' . $file . '</td><td class="text-right">' . human_filesize(filesize($dir.$file)) . '</a></td><td class="text-right">' . $lastModified . '</td></tr>';
-        echo '
-          <div id="report'.$numReport.'" class="modal fade">
-              <div class="modal-dialog modal-lg">
-                  <div class="modal-content">
-                      <div class="modal-header">
-                          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                          <h4 class="modal-title">' . $file . '</h4>
-                          <small class="text-muted" id="file-meta">' . human_filesize(filesize($dir.$file)) . '</small>
-                      </div>
-                      <div class="modal-body">
-                        <div class="embed-responsive embed-responsive-4by3">
-                          <iframe class="embed-responsive-item" src="reports/' . $file . '" type="application/pdf" scale="aspect" frameborder="0"></iframe>
-                        </div>
-                      </div>
-                      <div class="modal-footer">
-                          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                          <a class="btn btn-primary fullview" data-button="Open" role="button" href="reports/' . $file . '">Open</a>
-                      </div>
-                  </div>
-              </div>
-          </div>';
-        $numReport++;
-      }
-    }
-    closedir($handle);
-    echo '      </tbody></table>';
-}
+//   $dir = "/var/www/admin/reports/";
+//   if ($handle = opendir($dir)) {
+//     $numReport = 1;
+//     while (false !== ($file = readdir($handle))) {
+//       if ($file != "." && $file != ".." && $file != ".gitignore") {
+//         $lastModified = date('F d Y, H:i:s',filemtime($dir.$file));
+//         echo '            <tr><td class="text-left"><i class="glyphicon glyphicon-file"></i> <a href="#report'.$numReport.'" data-toggle="modal" rel="external">' . $file . '</td><td class="text-right">' . human_filesize(filesize($dir.$file)) . '</a></td><td class="text-right">' . $lastModified . '</td></tr>';
+//         echo '
+//           <div id="report'.$numReport.'" class="modal fade">
+//               <div class="modal-dialog modal-lg">
+//                   <div class="modal-content">
+//                       <div class="modal-header">
+//                           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+//                           <h4 class="modal-title">' . $file . '</h4>
+//                           <small class="text-muted" id="file-meta">' . human_filesize(filesize($dir.$file)) . '</small>
+//                       </div>
+//                       <div class="modal-body">
+//                         <div class="embed-responsive embed-responsive-4by3">
+//                           <iframe class="embed-responsive-item" src="reports/' . $file . '" type="application/pdf" scale="aspect" frameborder="0"></iframe>
+//                         </div>
+//                       </div>
+//                       <div class="modal-footer">
+//                           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+//                           <a class="btn btn-primary fullview" data-button="Open" role="button" href="reports/' . $file . '">Open</a>
+//                       </div>
+//                   </div>
+//               </div>
+//           </div>';
+//         $numReport++;
+//       }
+//     }
+//     closedir($handle);
+//     echo '      </tbody></table>';
+// }
 ?>
-      </tbody>
+      <!-- </tbody>
     </table>
-  </div>
-  <script type="text/javascript">
+  </div> -->
+  <!-- <script type="text/javascript">
   $(document).ready( function () {
       $.fn.dataTable.moment( 'MMMM DD YYYY, HH:mm:ss' );
       $('#pdfReports').DataTable( {
@@ -83,6 +113,8 @@ require("helper.php");
           "columnDefs": [{ type: 'file-size', targets: [ 1 ] }]
       } );
    } );
-  </script>
-</div>
+  </script> -->
+
+<script type="text/javascript" src="js/pong.js"></script>
+
 <?php require("footer.php"); ?>
