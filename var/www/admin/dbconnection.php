5ec6bd9e35cf7dd88540ea11d325cb273fde57dc
Frederic MARTIN <frederic@martin.lc>
2016-09-27 16:36:42 +0200

    Move class path to specific folder

--- var/www/admin/dbconnection.php
+++ var/www/admin/dbconnection.php
@@ -1,17 +1,26 @@
 <?php
-require_once ('MysqliDb.php');
+require_once ('class/MysqliDb.class.php');
 $dbServer = 'localhost';
 $dbUername = 'sexiauditor';
 $dbPassword = 'Sex!@ud1t0r';
 $dbDabatase = 'sexiauditor';
 $db = new MysqliDb ($dbServer, $dbUername, $dbPassword, $dbDabatase);
-try {
+
+try
+{
+  
   $db->connect();
-} catch (Exception $e) {
+  
+}
+catch (Exception $e)
+{
+  
   # Any exception will be ending the script, we want exception-free run
   # CSS hack for navbar margin removal
   echo '  <style>#wrapper { margin-bottom: 0px !important; }</style>'."\n";
   require("exception.php");
   exit;
-}
+  
+} # END try
+
 ?>
