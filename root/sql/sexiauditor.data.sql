18690c526d4f1d3a9e7eaa33c4aeef32617223ed
Frederic MARTIN <frederic@martin.lc>
2016-10-03 10:12:35 +0200

    Update sexiauditor.data.sql

--- root/sql/sexiauditor.data.sql
+++ root/sql/sexiauditor.data.sql
@@ -46,7 +46,8 @@ INSERT INTO `config` (`id`, `configid`, `type`, `label`, `value`) VALUES
 	(23, 'sexigrafNode', 2, 'SexiGraf node IP or FQDN (used for Capacity Planning)', 'sexigraf.sexibyt.es'),
 	(24, 'capacityPlanningDays', 4, 'Number of days Capacity Planning will used for computationcalculation of \'Days Left\' value', '7'),
 	(25, 'showInfinite', 1, 'Does Capacity Planning display clusters with \'Infinite\' days left ?', 'enable'),
-	(26, 'showDebug', 1, 'Display debug log in log files (careful, it is really verbose)', 'disable');
+	(26, 'showDebug', 1, 'Display debug log in log files (careful, it is really verbose)', 'disable'),
+	(27, 'anonymousROInventory', 1, 'Will allow anonymous access to a read-only inventory', 'disable');
 /*!40000 ALTER TABLE `config` ENABLE KEYS */;
 
 -- Dumping data for table sexiauditor.configtype: ~2 rows (approximately)
