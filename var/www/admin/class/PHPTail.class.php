e49ab3454531a098226af856d35514f98e8a68cf
Frederic MARTIN <frederic@martin.lc>
2016-10-06 10:47:21 +0200

    Switch to lang class usage

--- var/www/admin/class/PHPTail.class.php
+++ var/www/admin/class/PHPTail.class.php
@@ -118,7 +118,7 @@ class PHPTail
 <![endif]-->
 
 <script src="js/jquery-3.1.0.min.js"></script>
-
+<script src="js/bootstrap.min.js"></script>
 <script type="text/javascript">
   /* <![CDATA[ */
   //Last know size of the file
@@ -205,7 +205,7 @@ class PHPTail
 	</nav>
   <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
 		<p class="navbar-text navbar-left" style="padding-top: 0px;" id="current"></p>
-    <div class="navbar-brand">SexiAuditor</div>
+    <div class="navbar-brand"><a href="index.php">SexiAuditor</a></div>
       <ul class="nav navbar-top-links navbar-right">
         <li>
   				<a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="true">
@@ -247,50 +247,50 @@ else
               <div class="row">
                 <div class="<?php echo $widthColumn; ?>">
                   <ul class="multi-column-dropdown">
-                    <li><a href="index.php"><i class="glyphicon glyphicon-map-marker glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('USERDASHBOARD'); ?></a></li>
+                    <li><a href="index.php"><i class="glyphicon glyphicon-map-marker glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('USERDASHBOARD'); ?></a></li>
                     <li class="divider"></li>
-                    <li class="importantLabel"><a href="inv.php"><i class="glyphicon glyphicon-list-alt glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('VMINVENTORY'); ?></a></li>
-                    <li class="importantLabel"><a href="invhost.php"><i class="glyphicon glyphicon-list-alt glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('HOSTINVENTORY'); ?></a></li>
-                    <li><a href="capacityplanning.php"><i class="glyphicon glyphicon-signal glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('CAPACITYPLANNING'); ?></a></li>
+                    <li class="importantLabel"><a href="inv.php"><i class="glyphicon glyphicon-list-alt glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('VMINVENTORY'); ?></a></li>
+                    <li class="importantLabel"><a href="invhost.php"><i class="glyphicon glyphicon-list-alt glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('HOSTINVENTORY'); ?></a></li>
+                    <li><a href="capacityplanning.php"><i class="glyphicon glyphicon-signal glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('CAPACITYPLANNING'); ?></a></li>
                     <li class="divider"></li>
-                    <li><a href="check-vsan.php"><img src="images/vc-vsan.gif" class="glyphicon-custom" /> <?php echo $sexilang->getLocaleText('VSANCHECKS'); ?></a></li>
-                    <li><a href="check-vcenter.php"><img src="images/vc-vcenter.gif" class="glyphicon-custom" /> <?php echo $sexilang->getLocaleText('VCENTERCHECKS'); ?></a></li>
-                    <li><a href="check-cluster.php"><img src="images/vc-cluster.gif" class="glyphicon-custom" /> <?php echo $sexilang->getLocaleText('CLUSTERCHECKS'); ?></a></li>
-                    <li><a href="check-host.php"><img src="images/vc-host.gif" class="glyphicon-custom" /> <?php echo $sexilang->getLocaleText('HOSTCHECKS'); ?></a></li>
-                    <li><a href="check-datastore.php"><img src="images/vc-datastore.gif" class="glyphicon-custom" /> <?php echo $sexilang->getLocaleText('DATASTORECHECKS'); ?></a></li>
-                    <li><a href="check-network.php"><img src="images/vc-network.gif" class="glyphicon-custom" /> <?php echo $sexilang->getLocaleText('NETWORKCHECKS'); ?></a></li>
-                    <li><a href="check-vm.php"><img src="images/vc-vm.gif" class="glyphicon-custom" /> <?php echo $sexilang->getLocaleText('VMCHECKS'); ?></a></li>
+                    <li><a href="check-vsan.php"><img src="images/vc-vsan.gif" class="glyphicon-custom" /> <?php echo $this->sexilang->getLocaleText('VSANCHECKS'); ?></a></li>
+                    <li><a href="check-vcenter.php"><img src="images/vc-vcenter.gif" class="glyphicon-custom" /> <?php echo $this->sexilang->getLocaleText('VCENTERCHECKS'); ?></a></li>
+                    <li><a href="check-cluster.php"><img src="images/vc-cluster.gif" class="glyphicon-custom" /> <?php echo $this->sexilang->getLocaleText('CLUSTERCHECKS'); ?></a></li>
+                    <li><a href="check-host.php"><img src="images/vc-host.gif" class="glyphicon-custom" /> <?php echo $this->sexilang->getLocaleText('HOSTCHECKS'); ?></a></li>
+                    <li><a href="check-datastore.php"><img src="images/vc-datastore.gif" class="glyphicon-custom" /> <?php echo $this->sexilang->getLocaleText('DATASTORECHECKS'); ?></a></li>
+                    <li><a href="check-network.php"><img src="images/vc-network.gif" class="glyphicon-custom" /> <?php echo $this->sexilang->getLocaleText('NETWORKCHECKS'); ?></a></li>
+                    <li><a href="check-vm.php"><img src="images/vc-vm.gif" class="glyphicon-custom" /> <?php echo $this->sexilang->getLocaleText('VMCHECKS'); ?></a></li>
                   </ul>
                 </div>
   <?php if (isset($_SESSION['role']) && $_SESSION['role'] == '1'): ?>
                 <div class="<?php echo $widthColumn; ?>">
                   <ul class="multi-column-dropdown">
-                    <li><a href="admin.php"><i class="glyphicon glyphicon-map-marker glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('ADMINDASHBOARD'); ?></a></li>
+                    <li><a href="admin.php"><i class="glyphicon glyphicon-map-marker glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('ADMINDASHBOARD'); ?></a></li>
                     <li class="divider"></li>
-                    <li><a href="credstore.php"><i class="glyphicon glyphicon-briefcase glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('CREDENTIALSTORE'); ?></a></li>
-                    <li><a href="updater.php"><i class="glyphicon glyphicon-hdd glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('PACKAGEUPDATER'); ?></a></li>
-                    <li><a href="moduleselector.php"><i class="glyphicon glyphicon-check glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('MODULESELECTOR'); ?></a></li>
-                    <li><a href="showlog.php"><i class="glyphicon glyphicon-search glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('LOGVIEWER'); ?></a></li>
-                    <li><a href="timetobuild.php"><i class="glyphicon glyphicon-time glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('TIMETOBUILD'); ?></a></li>
-                    <li><a href="bundle.php"><i class="glyphicon glyphicon-floppy-disk glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('ESXBUNDLE'); ?></a></li>
+                    <li><a href="credstore.php"><i class="glyphicon glyphicon-briefcase glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('CREDENTIALSTORE'); ?></a></li>
+                    <li><a href="updater.php"><i class="glyphicon glyphicon-hdd glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('PACKAGEUPDATER'); ?></a></li>
+                    <li><a href="moduleselector.php"><i class="glyphicon glyphicon-check glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('MODULESELECTOR'); ?></a></li>
+                    <li><a href="showlog.php"><i class="glyphicon glyphicon-search glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('LOGVIEWER'); ?></a></li>
+                    <li><a href="timetobuild.php"><i class="glyphicon glyphicon-time glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('TIMETOBUILD'); ?></a></li>
+                    <li><a href="bundle.php"><i class="glyphicon glyphicon-floppy-disk glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('ESXBUNDLE'); ?></a></li>
                   </ul>
                 </div>
   <?php endif; ?>
                 <div class="<?php echo $widthColumn; ?>">
                   <ul class="multi-column-dropdown">
   <?php if (isset($_SESSION['role']) && $_SESSION['role'] == '1'): ?>
-                    <li><a href="config.php"><i class="glyphicon glyphicon-pencil glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('MODULESETTINGS'); ?></a></li>
-                    <li><a href="cpgroup.php"><i class="glyphicon glyphicon-th-list glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('CAPACITYPLANNINGGROUP'); ?></a></li>
-                    <li><a href="users.php"><i class="glyphicon glyphicon-user glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('USERSMANAGEMENT'); ?></a></li>
-                    <li><a href="import-export.php"><i class="glyphicon glyphicon-transfer glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('IMPORTEXPORT'); ?></a></li>
+                    <li><a href="config.php"><i class="glyphicon glyphicon-pencil glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('MODULESETTINGS'); ?></a></li>
+                    <li><a href="cpgroup.php"><i class="glyphicon glyphicon-th-list glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('CAPACITYPLANNINGGROUP'); ?></a></li>
+                    <li><a href="users.php"><i class="glyphicon glyphicon-user glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('USERSMANAGEMENT'); ?></a></li>
+                    <li><a href="import-export.php"><i class="glyphicon glyphicon-transfer glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('IMPORTEXPORT'); ?></a></li>
   <?php endif; ?>
-                    <li><a href="onetime.php"><i class="glyphicon glyphicon-book glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('ONETIMEREPORT'); ?></a></li>
-                    <li><a href="pdfreports.php"><i class="glyphicon glyphicon-print glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('PDFREPORTS'); ?></a></li>
-                    <li><a href="status.php"><i class="glyphicon glyphicon-screenshot glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('SCHEDULERSTATUS'); ?></a></li>
+                    <li><a href="onetime.php"><i class="glyphicon glyphicon-book glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('ONETIMEREPORT'); ?></a></li>
+                    <li><a href="pdfreports.php"><i class="glyphicon glyphicon-print glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('PDFREPORTS'); ?></a></li>
+                    <li><a href="status.php"><i class="glyphicon glyphicon-screenshot glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('SCHEDULERSTATUS'); ?></a></li>
                     <li class="divider"></li>
-                    <li><a href="passwordupdate.php"><i class="glyphicon glyphicon-asterisk glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('UPDATEPASSWORD'); ?></a></li>
-                    <li><a href="about.php"><i class="glyphicon glyphicon-question-sign glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('ABOUTSEXIAUDITOR'); ?></a></li>
-                    <li><a href="logout.php"><i class="glyphicon glyphicon-log-out glyphicon-custom"></i> <?php echo $sexilang->getLocaleText('LOGOUT'); ?></a></li>
+                    <li><a href="passwordupdate.php"><i class="glyphicon glyphicon-asterisk glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('UPDATEPASSWORD'); ?></a></li>
+                    <li><a href="about.php"><i class="glyphicon glyphicon-question-sign glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('ABOUTSEXIAUDITOR'); ?></a></li>
+                    <li><a href="logout.php"><i class="glyphicon glyphicon-log-out glyphicon-custom"></i> <?php echo $this->sexilang->getLocaleText('LOGOUT'); ?></a></li>
                   </ul>
                 </div>
               </div>
@@ -302,7 +302,6 @@ else
 	<div class="contents">
 		<div id="results" class="results"></div>
 	</div>
-	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
 <?php
