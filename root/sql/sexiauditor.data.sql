0a88ba2bff0b780422d89d1314605229246c8102
Frederic MARTIN <frederic@martin.lc>
2016-10-24 16:31:51 +0200

    Update sexiauditor.data.sql

--- root/sql/sexiauditor.data.sql
+++ root/sql/sexiauditor.data.sql
@@ -215,7 +215,7 @@ INSERT INTO `modules` (`id`, `module`, `type`, `displayName`, `version`, `descri
 	(61, 'VSANHealthCheck', 'report', 'VSAN Health Check', 0, 'Display VSAN information about Health Check.', 1, 'off'),
 	(62, 'alarms', 'report', 'Alarms', 1, 'Will display triggered alarms on objects level with status and time of creation.', 8, 'daily'),
 	(63, 'hostBuildPivot', 'report', 'Host ESXBuild Pivot Table', 1, 'Will display a list of all ESX build group by count.', 7, 'daily'),
-	(64, 'capacityPlanningReport', 'action', 'Capacity Planning Report', 0, 'Send Capacity Planning Report by mail', 8, 'hourly'),
+	(64, 'capacityPlanningReport', 'action', 'Capacity Planning Report', 1, 'Send Capacity Planning Report by mail', 8, 'hourly'),
 	(999, 'mailAlert', 'action', 'Mail Alert', 0, 'Will send an email with all defective checks', 8, 'daily');
 /*!40000 ALTER TABLE `modules` ENABLE KEYS */;
 
