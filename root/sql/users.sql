a9923ef76b7784be076f0dd9fb6250d4aff98d21
Frederic MARTIN <frederic@martin.lc>
2017-01-04 14:03:53 +0100

    Update users.sql

--- root/sql/users.sql
+++ root/sql/users.sql
@@ -1,4 +1,6 @@
 # Adding default user for db connection and grant privileges
 CREATE USER 'sexiauditor'@'localhost' IDENTIFIED BY 'Sex!@ud1t0r';
 GRANT ALL PRIVILEGES ON sexiauditor.* TO 'sexiauditor'@'localhost';
+CREATE USER 'api'@'localhost' IDENTIFIED BY 'Sex!@ud1t0rR0x';
+GRANT SELECT ON api.* TO 'sexiauditor'@'localhost';
 FLUSH PRIVILEGES;
